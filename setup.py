67c612dbb374ba8b5fcc0e10fc2e49c7e2b29760
Mani-99yards <gounimanikumar12@gmail.com>
2025-02-27 16:14:00 -0500

    Bumped version to 1.0.1 for PyPI release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ceilr-py-sdk",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=["requests"],
     description="CeilR Python SDK for feature access, usage tracking, and entitlements",
